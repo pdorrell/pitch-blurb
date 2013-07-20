3d5b6df7104d3240fc3fb8da801f6047fca06994
Philip Dorrell <philip@Cangallo-Ubuntu.(none)>
2013-07-21 10:54:07 +1200

    readSourceFile

--- spec/parser_spec.rb
+++ spec/parser_spec.rb
@@ -4,9 +4,13 @@ module PitchBlurbs
   
   describe 'parse a pitch blurb' do
     
+    def readSourceFile(relativePath)
+      File.open(File.join(File.dirname(__FILE__), relativePath)).read
+    end
+    
     it "parses a pitch blurb" do
       parser = PitchBlurbParser.new
-      pitchBlurbSource = File.open(File.join(File.dirname(__FILE__), "input/frog.blurb")).read
+      pitchBlurbSource = readSourceFile("input/frog.blurb")
       pitchBlurb = parser.parseBlurb(pitchBlurbSource)
       expectedPitchBlurb = Blurb.new(title: "My Frog", url: "frog.html", 
                                      blurb: "it eats flies", 
